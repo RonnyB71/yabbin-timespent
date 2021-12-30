e161c7d125d4a1035fe4e01ed2b8318e0b66ebcf
Ronny Birkeli <ronny.birkeli@digdir.no>
2021-12-30 23:07:18 +0100

    chore: add azure resources for hosting docker container

---
+++ deployment/main.tf
@@ -0,0 +1,24 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.65"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "default" {
+  name     = "${var.name}-${var.environment}-rg"
+  location = "${var.location}"
+  tags = {
+    Environment = "${var.environment}"
+    Team = "DevOps-1"
+  }
+}
---
+++ deployment/variables.tf
@@ -0,0 +1,35 @@
+variable "name" {
+  type        = string
+  description = "Location of the azure resource group."
+  default     = "timespent"
+}
+
+variable "environment" {
+  type        = string
+  description = "Name of the deployment environment"
+  default     = "prod"
+}
+
+variable "location" {
+  type        = string
+  description = "Location to deploy the resoruce group"
+  default     = "norwayeast"
+}
+
+variable "dns_prefix" {
+  type        = string
+  description = "A prefix for any dns based resources"
+  default     = "yts"
+}
+
+variable "plan_tier" {
+  type        = string
+  description = "The tier of app service plan to create"
+  default     = "Standard"
+}
+
+variable "plan_sku" {
+  type        = string
+  description = "The sku of app service plan to create"
+  default     = "S1"
+}
