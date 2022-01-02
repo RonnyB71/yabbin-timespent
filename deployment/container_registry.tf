d61e6e870dc4ee10e200503df804270c0af0b9f5
Ronny Birkeli <ronny.birkeli@digdir.no>
2022-01-02 21:29:13 +0100

    chore: enabled admin user for container registry

--- deployment/container_registry.tf
+++ deployment/container_registry.tf
@@ -3,4 +3,5 @@ resource "azurerm_container_registry" "acr" {
   location            = "${azurerm_resource_group.default.location}"
   resource_group_name = "${azurerm_resource_group.default.name}"
   sku                 = "Basic"
+  admin_enabled       = true
 }
