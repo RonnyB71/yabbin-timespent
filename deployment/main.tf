a8a11955b2f9e44e0d137abfcfa3b1b3568e7abd
Ronny Birkeli <ronny.birkeli@digdir.no>
2022-01-21 22:58:53 +0100

    chore: atempt to add authenticaiont to backend state store 13

--- deployment/main.tf
+++ deployment/main.tf
@@ -16,7 +16,6 @@ terraform {
         client_secret = "${var.agent_client_secret}"
         subscription_id = "${var.subscription_id}"
         tenant_id = "${var.tenant_id}"
-        
     }
 
   required_version = ">= 0.14.9"
